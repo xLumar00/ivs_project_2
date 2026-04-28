8688759e1c8e1f4ed3169d18baf931b3ae3b0b6d
xvojtat00 <timotej.vojtas@gmail.com>
2026-04-28 11:52:00 +0200

    feat: implement sample standard deviation formula
    - Validated by 12-test suite (all passed)
    - Handles edge cases
    - Uses the formula sqrt((sum_sq - N*arith_mean^2)/(N-1))

--- src/stddev.py
+++ src/stddev.py
@@ -6,9 +6,9 @@ def stddev_calc(n, sum_numbers, sum_squares):
         return 0.0
         #the standard deviation of 2 elements is always 0
 
-
-
-    #TODO THE FINALISATION OF THE RESULT (sqrt beside other things)
+    arithmetic_mean = sum_numbers / n
+    s = ((sum_squares - n*arithmetic_mean**2)/(n-1))**(0.5) #TODO CALL MATHLIB ONCE READY
+    return max(0, s)
 
 
 def main():
@@ -19,20 +19,6 @@ def main():
     for line in sys.stdin:
         print(line, end="")
 
-        char_seqs = line.split()
-        for char_seq in char_seqs:
-            try:
-                number = float(char_seq)
-            except ValueError:
-                #not a number
-                continue
-            else:
-                sum_numbers += number
-                sum_squares += 0 #TODO ADD CALL FOR MATHLIB
-                count += 1
-
-    result = stddev_calc(count, sum_of_numbers, sum_of_squares)
-    print(result)
 
 if __name__ == "__main__":
     main()
