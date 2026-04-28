8753432c78b1ad002cd49bf9d73a2a10e5036e7c
xvojtat00 <timotej.vojtas@gmail.com>
2026-04-28 10:22:01 +0200

    style: apply reviewer suggestions for src/stddev.py

--- src/stddev.py
+++ src/stddev.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import sys
 
+def stddev_calc(count, sum_nums, sum_sq):
+    # Placeholder for TDD
+    return 22.22 #random return value since 0.0 would actually be correct for some tests
+
 def main():
     count = 0
     sumOfNumbers = 0.0
