e16984207c3a9773db4cdacda233c606c4927ede
xvojtat00 <timotej.vojtas@gmail.com>
2026-04-28 11:32:55 +0200

    fix: resolve merge conflict and restore the work in progress logic

--- src/stddev.py
+++ src/stddev.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 import sys
 
-def stddev_calc(count, sum_nums, sum_sq):
-    # Placeholder for TDD
-    return 22.22 #random return value since 0.0 would actually be correct for some tests
+def stddev_calc(n, sum_numbers, sum_squares):
+    if n < 2:
+        return 0.0
+        #the standard deviation of 2 elements is always 0
+
+
+
+    #TODO THE FINALISATION OF THE RESULT (sqrt beside other things)
+
 
 def main():
     count = 0
-    sumOfNumbers = 0.0
-    sumOfSquares = 0.0
+    sum_of_numbers = 0.0
+    sum_of_squares = 0.0
+
     for line in sys.stdin:
         print(line, end="")
 
+        char_seqs = line.split()
+        for char_seq in char_seqs:
+            try:
+                number = float(char_seq)
+            except ValueError:
+                #not a number
+                continue
+            else:
+                sum_numbers += number
+                sum_squares += 0 #TODO ADD CALL FOR MATHLIB
+                count += 1
+
+    result = stddev_calc(count, sum_of_numbers, sum_of_squares)
+    print(result)
 
 if __name__ == "__main__":
     main()
