d78d1594c9c32addff5c0828cfcd86f4f26e1063
TeaMeMeh <150188022+TeaMeMeh@users.noreply.github.com>
2026-04-28 11:10:05 +0200

    feat(profiling): implement stddev structure and unit tests
    * Replaced C profiling skeleton with Python implementation.
    * Added stddev.py with function signatures and placeholders for TDD.
    * Implemented a comprehensive test suite in tests_stddev.py (12 test cases).
    * Added stddev_num_generator.py for profiling data generation.
    * Updated .gitignore to exclude Python bytecode and profiling outputs.

---
+++ src/stddev.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+def stddev_calc(count, sum_nums, sum_sq):
+    # Placeholder for TDD
+    return 22.22 #random return value since 0.0 would actually be correct for some tests
+
+def main():
+    count = 0
+    sumOfNumbers = 0.0
+    sumOfSquares = 0.0
+    for line in sys.stdin:
+        print(line, end="")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/stddev_num_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+import random
+
+
+def generate(n):
+    for i in range(n):
+        num = random.uniform(-999999, 999999)
+        sys.stdout.write(f"{num}\n")
+
+if __name__ == "__main__":
+    count = 1000000
+    if len(sys.argv) > 1:
+        try: 
+            count = int(sys.argv[1])
+        except ValueError:
+            pass
+
+    generate(count)
