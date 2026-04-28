793bf49e3e279585cacf8cf31d01b19047613cbd
timolukotka <timolukotka@gmail.com>
2026-04-28 11:48:46 +0200

    feat/add implementation and error checks for factorial , power and square

--- src/math_lib.py
+++ src/math_lib.py
@@ -29,24 +29,59 @@ def div(a, b):
 
 def factorial(n):
     """(n!)Returns n factorial"""
-    pass
+    _validate_numbers(n)
+    if (0 > n) : 
+        raise ValueError("exponent must be natural number")
+    
+    if not (isinstance(n, int)):
+        raise ValueError("exponent must be natural number")
+    
+    factorial = 1
+    while (n > 0):
+        factorial = factorial * n
+        n-=1
+    return factorial
 
 def power(base, exponent):
-    """natural exponents only!!! returns base raised to the power of exponent """
-    pass
+    """non-negative integer exponents only!!! returns base raised to the power of exponent """
+    _validate_numbers(base, exponent)
+    
+    if (0 > exponent) : 
+        raise ValueError("exponent must be natural number")
+    
+    if not (isinstance(exponent, int)):
+        raise ValueError("exponent must be natural number")
+    
+    if exponent == 0:
+        return 1
+    
+    counter = 1
+    result = base
+    for counter in range(1,exponent):
+        result *= base
+       
+    return result
+    
 
 def root(base, degree):
     """Returns the nth root of the value."""
-    pass 
+    _validate_numbers(base, degree)
+    epsilon = 0.0000001 #tolerance
+
+    pass
 
 def square(a):
     """(a^2) Returns a squared ."""
-    pass
+    _validate_numbers(a)
+    
+    return a*a 
 
 def sqrt(a):
     """Returns square root of a"""
+    _validate_numbers(a)
     pass
 
 def inverse(x):
     """(1/x) Returns ONE over X"""
+    _validate_numbers(x)
     pass 
