e4f1e33970e5811c199db527e712ff2815005206
xvojtat00 <timotej.vojtas@gmail.com>
2026-04-29 11:16:59 +0200

    feat: implement stddev streaming logic
    - Used sys.stdin for efficient processing
    - Passed all 6 functional input tests
    - Verified accuracy against 'Calculator.net' reference values
    - Removed old calculation TODOs and replaced them for the calls for our math_lib functions

--- src/stddev.py
+++ src/stddev.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import sys
+import math_lib
 
 def stddev_calc(n, sum_numbers, sum_squares):
     if n < 2:
         return 0.0
-        #the standard deviation of 2 elements is always 0
+        #if the sequence has less than 2 elements, we will return 0.0 as the StdDev
 
-    arithmetic_mean = sum_numbers / n
-    s = ((sum_squares - n*arithmetic_mean**2)/(n-1))**(0.5) #TODO CALL MATHLIB ONCE READY
-    return max(0, s)
+    arithmetic_mean = math_lib.div(sum_numbers, n)
+    s = math_lib.sqrt(math_lib.div((sum_squares - n*math_lib.square(arithmetic_mean)), (n-1)))
+    return max(0.0, s) #if the result is negative and close to 0, we will return 0.0
 
 
 def main():
@@ -17,7 +18,22 @@ def main():
     sum_of_squares = 0.0
 
     for line in sys.stdin:
-        print(line, end="")
+        char_seqs = line.split()
+        for char_seq in char_seqs:
+            char_seq.replace(',', '')
+
+            try:
+                number = float(char_seq)
+            except ValueError:
+                #not a number
+                continue
+            else:
+                sum_of_numbers += number
+                sum_of_squares += math_lib.square(number, 2)
+                count += 1
+
+    result = stddev_calc(count, sum_of_numbers, sum_of_squares)
+    print(result)
 
 
 if __name__ == "__main__":
