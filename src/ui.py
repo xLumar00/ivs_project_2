e3cf19935db702bc5f25bc16491a2e53b295c7fb
Matúš Heldi <matusheldi@2782429992.wifi.kn.vutbr.cz>
2026-04-30 05:49:49 +0200

    Bug fixed with factorial

--- src/ui.py
+++ src/ui.py
@@ -18,12 +18,12 @@ def can_add_operator():
 
     if text == "":
         return False
-    
+
     forbidden_suffixes = ("+", "-", "*", "/", ".", "^", "sq", "sqrt", "rt", "1/x")
-       
+
     if text.endswith(forbidden_suffixes):
         return False
-    
+
     return True
 
 
@@ -36,7 +36,7 @@ def auto_compute():
     forbidden_suffixes = ("+", "-", "*", "/", ".", "^", "sq", "sqrt", "rt", "1/x")
     if text.endswith(forbidden_suffixes):
         return
-    
+
     text_to_check = text[1:] if text.startswith("-") else text
 
     for op in ["+", "-", "*", "/", "^", "sq", "rt", "1/x", "!"]:
@@ -45,7 +45,7 @@ def auto_compute():
             break
 
 
-def open_help():  
+def open_help():
     """!
     @brief Opens a secondary top-level window containing a help guide.
     @details Displays instructions for the buttons, keyboard shortcuts, and general information about errors.
@@ -54,10 +54,10 @@ def open_help():
     help_window.title("Hint")
     help_window.geometry("400x500")
     help_window.attributes("-topmost", True)
-    
+
     textbox = ctk.CTkTextbox(help_window, font=("Roboto", 15), wrap="word", fg_color="transparent")
     textbox.pack(fill="both", expand=True, padx=20, pady=20)
-    
+
     help_text = """ WELCOME TO THE CALCULATOR
 
 Buttons & Functions:
@@ -78,6 +78,7 @@ You can fully control the calculator using your keyboard.
     textbox.insert("0.0", help_text)
     textbox.configure(state="disabled")
 
+
 app = ctk.CTk()
 app.title("Calculator")
 app.geometry("450x700")
@@ -87,18 +88,36 @@ app.configure(fg_color="#121212")
 
 for i in range(4):
     app.grid_columnconfigure(i, weight=1, uniform="equal_cons")
-for i in range (1, 7):
+for i in range(1, 7):
     app.grid_rowconfigure(i, weight=1, uniform="equal_rows")
 
 font_display = ("Roboto", 55, "bold")
-font_button = ("Roboto", 24,"bold")
+font_button = ("Roboto", 24, "bold")
 
 theme_num = {"fg_color": "#2b2b2b", "hover_color": "#404040", "text_color": "white", "font": font_button}
 theme_op = {"fg_color": "#D32F2F", "hover_color": "#F44336", "text_color": "white", "font": font_button}
 theme_spec = {"fg_color": "#E0E0E0", "hover_color": "#FFFFFF", "text_color": "black", "font": font_button}
 
+display_var = ctk.StringVar()
 
-display = ctk.CTkEntry(app, placeholder_text="0", justify="right", state="readonly", font=font_display, height=90, fg_color="#1e1e1e", border_color="#D32F2F")
+
+def auto_resize_font(*args):
+    text_length = len(display_var.get())
+
+    if text_length < 11:
+        display.configure(font=("Roboto", 55, "bold"))
+    elif text_length < 16:
+        display.configure(font=("Roboto", 40, "bold"))
+    elif text_length < 22:
+        display.configure(font=("Roboto", 30, "bold"))
+    else:
+        display.configure(font=("Roboto", 22, "bold"))
+
+
+display_var.trace_add("write", auto_resize_font)
+
+display = ctk.CTkEntry(app, textvariable=display_var, placeholder_text="0", justify="right", state="readonly",
+                       font=font_display, height=90, fg_color="#1e1e1e", border_color="#D32F2F")
 display.grid(row=0, column=0, columnspan=4, padx=10, pady=(20, 20), sticky="nsew")
 
 
@@ -110,28 +129,48 @@ def key_pressed(event):
     char = event.char
     keysym = event.keysym
 
-    if char == '1' : button1()
-    elif char == '2' : button2()
-    elif char == '3' : button3()
-    elif char == '4' : button4()
-    elif char == '5' : button5()
-    elif char == '6' : button6()
-    elif char == '7' : button7()
-    elif char == '8' : button8()
-    elif char == '9' : button9()
-    elif char == '0' : button0()
+    if char == '1':
+        button1()
+    elif char == '2':
+        button2()
+    elif char == '3':
+        button3()
+    elif char == '4':
+        button4()
+    elif char == '5':
+        button5()
+    elif char == '6':
+        button6()
+    elif char == '7':
+        button7()
+    elif char == '8':
+        button8()
+    elif char == '9':
+        button9()
+    elif char == '0':
+        button0()
 
-    elif char == '+': button_add()
-    elif char == '-': button_sub()
-    elif char == '*': button_mul()
-    elif char == '/': button_div()
-    elif char == '!': button_factorial()
-    elif char == '.' or char == ',': button_point()
+    elif char == '+':
+        button_add()
+    elif char == '-':
+        button_sub()
+    elif char == '*':
+        button_mul()
+    elif char == '/':
+        button_div()
+    elif char == '!':
+        button_factorial()
+    elif char == '.' or char == ',':
+        button_point()
 
-    elif keysym == 'Return' or keysym == 'KP_Enter' or char == '=': button_equals()
-    elif keysym == 'BackSpace': button_rm()
-    elif keysym == 'Escape': button_ac()
-    
+    elif keysym == 'Return' or keysym == 'KP_Enter' or char == '=':
+        button_equals()
+    elif keysym == 'BackSpace':
+        button_rm()
+    elif keysym == 'Escape':
+        button_ac()
+
+
 app.bind('<Key>', key_pressed)
 
 
@@ -145,7 +184,7 @@ def button_equals():
     forbidden_suffixes = ("+", "-", "*", "/", ".", "^", "rt")
     if expression.endswith(forbidden_suffixes):
         return
-    
+
     try:
         expression = expression.replace("--", "+")
         expression = expression.replace("+-", "-")
@@ -159,7 +198,7 @@ def button_equals():
             result = math_lib.mul(float(numbers[0]), float(numbers[1]))
 
         elif "ˇ2" in expression:
-            numbers = expression.split("ˇ2")       
+            numbers = expression.split("ˇ2")
             result = math_lib.square(float(numbers[0]))
 
         elif "^" in expression:
@@ -177,10 +216,10 @@ def button_equals():
         elif "-" in expression:
             numbers = expression.rsplit("-", 1)
             result = math_lib.sub(float(numbers[0]), float(numbers[1]))
-        
+
         else:
             result = expression
-        
+
         display.configure(state="normal")
         display.delete(0, "end")
         display.insert("end", str(result))
@@ -188,7 +227,7 @@ def button_equals():
 
     except Exception as e:
         display.configure(state="normal")
-        display.delete(0,"end")
+        display.delete(0, "end")
 
         error_msg = str(e)
 
@@ -205,6 +244,7 @@ def button_equals():
         display.insert("end", error_msg)
         display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="=", command=button_equals, **theme_op)
 button.grid(row=6, column=3, padx=5, pady=5, sticky="nsew")
 
@@ -216,7 +256,7 @@ def button_inverse():
     text = display.get()
     if text == "":
         return
-    
+
     last_number = text
     for operator in ["+", "-", "*", "/", "^", "rt"]:
         if operator in last_number:
@@ -236,6 +276,7 @@ def button_inverse():
         except Exception:
             pass
 
+
 button = ctk.CTkButton(app, text="1/x", command=button_inverse, **theme_spec)
 button.grid(row=2, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -248,7 +289,7 @@ def button_sq():
     text = display.get()
     if text == "":
         return
-    
+
     last_number = text
     for operator in ["+", "-", "*", "/", "^", "rt"]:
         if operator in last_number:
@@ -272,6 +313,7 @@ def button_sq():
         except Exception:
             pass
 
+
 button = ctk.CTkButton(app, text="√x", command=button_sq, **theme_spec)
 button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 
@@ -283,7 +325,7 @@ def button_square():
     text = display.get()
     if text == "":
         return
-    
+
     last_number = text
     for operator in ["+", "-", "*", "/", "^", "rt"]:
         if operator in last_number:
@@ -302,6 +344,7 @@ def button_square():
         except Exception:
             pass
 
+
 button = ctk.CTkButton(app, text="x²", command=button_square, **theme_spec)
 button.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
 
@@ -315,6 +358,7 @@ def button_root():
         display.configure(state="normal")
         display.insert("end", "rt")
         display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="ⁿ√x", command=button_root, **theme_spec)
 button.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
@@ -330,6 +374,7 @@ def button_power():
         display.insert("end", "^")
         display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="xⁿ", command=button_power, **theme_spec)
 button.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
 
@@ -343,6 +388,7 @@ def button_add():
         display.configure(state="normal")
         display.insert("end", "+")
         display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="+", command=button_add, **theme_op)
 button.grid(row=5, column=3, padx=5, pady=5, sticky="nsew")
@@ -360,17 +406,18 @@ def button_sub():
         display.configure(state="normal")
         display.insert("end", "-")
         display.configure(state="readonly")
-    
+
     elif text.endswith(("+", "-", "*", "/", "^", "rt", "sq", "sqrt", "1/x")):
         if not text.endswith("--"):
             display.configure(state="normal")
             display.insert("end", "-")
             display.configure(state="readonly")
-    
+
     elif can_add_operator() == True:
         display.configure(state="normal")
         display.insert("end", "-")
         display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="-", command=button_sub, **theme_op)
 button.grid(row=4, column=3, padx=5, pady=5, sticky="nsew")
@@ -383,9 +430,10 @@ def button_rm():
     current_text = display.get()
     if len(current_text) > 0:
         display.configure(state="normal")
-   
+
     display.delete(len(current_text) - 1, "end")
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="⌫", command=button_rm, **theme_spec)
 button.grid(row=1, column=3, padx=5, pady=5, sticky="nsew")
@@ -401,6 +449,7 @@ def button_div():
         display.insert("end", "/")
         display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="÷", command=button_div, **theme_op)
 button.grid(row=2, column=3, padx=5, pady=5, sticky="nsew")
 
@@ -415,6 +464,7 @@ def button_mul():
         display.insert("end", "*")
         display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="×", command=button_mul, **theme_op)
 button.grid(row=3, column=3, padx=5, pady=5, sticky="nsew")
 
@@ -427,6 +477,7 @@ def button_ac():
     display.delete(0, "end")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="AC", command=button_ac, **theme_spec)
 button.grid(row=1, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -438,7 +489,7 @@ def button_factorial():
     text = display.get()
     if text == "":
         return
-    
+
     last_number = text
     for operator in ["+", "-", "*", "/", "^", "rt"]:
         if operator in last_number:
@@ -446,7 +497,11 @@ def button_factorial():
 
     if last_number != "":
         try:
-            number = int(float(last_number))
+            raw_val = float(last_number)
+            if not raw_val.is_integer():
+                raise ValueError("Only integers for !")
+
+            number = int(raw_val)
             result = math_lib.factorial(number)
 
             new_text = text[:-len(last_number)] + str(result)
@@ -457,6 +512,7 @@ def button_factorial():
             display.configure(state="readonly")
         except Exception:
             pass
+
 
 button = ctk.CTkButton(app, text="!", command=button_factorial, **theme_spec)
 button.grid(row=6, column=0, padx=5, pady=5, sticky="nsew")
@@ -471,7 +527,7 @@ def button_point():
     for operator in ["+", "-", "*", "/", "^", "rt"]:
         if operator in last_number:
             last_number = last_number.split(operator)[-1]
-    
+
     if "." not in last_number:
         display.configure(state="normal")
 
@@ -481,6 +537,7 @@ def button_point():
             display.insert("end", ".")
 
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text=".", command=button_point, **theme_num)
 button.grid(row=6, column=2, padx=5, pady=5, sticky="nsew")
@@ -494,6 +551,7 @@ def button0():
     display.insert("end", "0")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="0", command=button0, **theme_num)
 button.grid(row=6, column=1, padx=5, pady=5, sticky="nsew")
 
@@ -505,6 +563,7 @@ def button1():
     display.configure(state="normal")
     display.insert("end", "1")
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="1", command=button1, **theme_num)
 button.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
@@ -518,6 +577,7 @@ def button2():
     display.insert("end", "2")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="2", command=button2, **theme_num)
 button.grid(row=5, column=1, padx=5, pady=5, sticky="nsew")
 
@@ -529,6 +589,7 @@ def button3():
     display.configure(state="normal")
     display.insert("end", "3")
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="3", command=button3, **theme_num)
 button.grid(row=5, column=2, padx=5, pady=5, sticky="nsew")
@@ -542,6 +603,7 @@ def button4():
     display.insert("end", "4")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="4", command=button4, **theme_num)
 button.grid(row=4, column=0, padx=5, pady=5, sticky="nsew")
 
@@ -553,6 +615,7 @@ def button5():
     display.configure(state="normal")
     display.insert("end", "5")
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="5", command=button5, **theme_num)
 button.grid(row=4, column=1, padx=5, pady=5, sticky="nsew")
@@ -566,6 +629,7 @@ def button6():
     display.insert("end", "6")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="6", command=button6, **theme_num)
 button.grid(row=4, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -577,6 +641,7 @@ def button7():
     display.configure(state="normal")
     display.insert("end", "7")
     display.configure(state="readonly")
+
 
 button = ctk.CTkButton(app, text="7", command=button7, **theme_num)
 button.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
@@ -590,6 +655,7 @@ def button8():
     display.insert("end", "8")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="8", command=button8, **theme_num)
 button.grid(row=3, column=1, padx=5, pady=5, sticky="nsew")
 
@@ -602,11 +668,12 @@ def button9():
     display.insert("end", "9")
     display.configure(state="readonly")
 
+
 button = ctk.CTkButton(app, text="9", command=button9, **theme_num)
 button.grid(row=3, column=2, padx=5, pady=5, sticky="nsew")
 
-
-help_button = ctk.CTkButton(app, text="?", width=15, height=15, font=("Roboto", 14, "bold"), fg_color="#7a7a7a", hover_color="#333333", command=open_help)
+help_button = ctk.CTkButton(app, text="?", width=15, height=15, font=("Roboto", 14, "bold"), fg_color="#7a7a7a",
+                            hover_color="#333333", command=open_help)
 
 help_button.place(x=20, y=30)
 
